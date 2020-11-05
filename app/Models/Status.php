113e176f29b2db342f5419b8aac15eec1f99050c
jiejia2009 <jiejia2009@gmail.com>
2020-11-05 16:40:23 +0800

    '删除微博'

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -21,6 +21,6 @@ class Status extends Model
      */
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 }
